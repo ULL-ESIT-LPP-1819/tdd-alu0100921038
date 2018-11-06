91f15184bf2f8e2b091d1413ab8c454c0605f751
Juan Martínez <alu0100921038@ull.edu.es>
2018-11-06 13:05:14 +0000

    Cuarta expectativa funcionando

--- lib/Alimentos.rb
+++ lib/Alimentos.rb
@@ -1,12 +1,13 @@
 require "prct06/version"
   class Alimentos
-    attr_reader :etiqueta, :grasas, :grasas_sat
+    attr_reader :etiqueta, :grasas, :grasas_sat, :hidratos
 
     #constructor
-    def initialize (etiqueta, grasas, grasas_sat)
+    def initialize (etiqueta, grasas, grasas_sat, hidratos)
 	@etiqueta = etiqueta
 	@grasas = grasas
 	@grasas_sat = grasas_sat
+	@hidratos = hidratos
     end
 
 end
