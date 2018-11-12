70eb2ca7ae53ce04b386cf05f6fbcc9f12311109
Juan Martínez <alu0100921038@ull.edu.es>
2018-11-12 11:30:00 +0000

    Clase creada y expectativas funcionando

--- lib/listaEnlazada.rb
+++ lib/listaEnlazada.rb
@@ -8,11 +8,115 @@ class Lista
         @tail = node
     end
 
-     def push_back(var)
+    def push_back(var)
         node = Node.new(var, nil, @tail)
         if(@tail != nil)
             @tail.sig = node
         end
         @tail = node
     end
+
+    def push_front(var)
+        node = Node.new(var, @head, nil)
+        if(@head != nil)
+            @head.prev = node
+        end
+        @head = node
+    end
+    
+    def front()
+        if(@head == nil)
+            return nil
+        end
+        return @head.datos
+    end
+    
+    def back()
+        if(@tail == nil)
+            return nil
+        end
+        return @tail.datos
+    end
+    
+    def pop_front()
+        node = @head
+        @head = node.sig
+        @head.prev = nil
+        if(node == nil)
+            return nil
+        end
+        return node.datos
+    end
+    
+    def pop_back()
+        node = @tail
+        @tail = node.prev
+        @tail.sig = nil
+        if(node == nil)
+            return nil
+        end
+        return node.datos
+    end
+    
+    def element(n)
+        node = @head
+        n.times do
+            node = node.sig
+        end
+        if(node == nil)
+            return nil
+        end
+        return node.datos
+    end
+    
+    def set_element(var, n)
+        pnode = @head
+        (n-1).times do
+            pnode = pnode.sig
+        end
+        if(pnode == @head)
+            push_front(var)
+        elsif(pnode == nil)
+            push_back(var)
+        else
+            nnode = pnode.sig
+            node = Node.new(var, nnode, pnode)
+            pnode.sig = node
+            nnode.prev = node
+        end
+    end
+    
+    def pop_element(n)
+        node = @head
+        n.times do
+            node = node.sig
+        end
+        if(node == nil)
+            return nil
+        else
+            pnode = node.prev
+            nnode = node.sig
+            if(pnode == nil)
+                return pop_front()
+            end
+            if(nnode == nil)
+                return pop_back()
+            end
+            pnode.sig = nnode
+            nnode.prev = pnode
+        end
+        return node.datos
+    end
+    
+    def to_s()
+        toprint ="{\n"
+        node = @head
+        while(node != nil) do
+            toprint += "#{node.datos}\n"
+            node = node.sig
+        end
+        toprint += "}"
+        return toprint
+    end
 end
+
--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -18,6 +18,66 @@ describe Lista do
     it "Debe tener cabeza" do
       expect(@l.head).to_not eq(nil) 
     end
-   end
-
+   it "Debe tener cola" do
+      expect(@l.tail).to_not eq(nil) 
+    end
+    it "Los nodos deben tener datos" do
+      expect(@l.head.datos).to_not eq(nil) 
+    end
+    it "Los nodos deben tener anterior" do
+      expect(@l.tail.prev).to_not eq(nil) 
+    end
+    it "Los nodos deben tener siguiente" do
+      expect(@l.head.sig).to_not eq(nil) 
+    end
+  end
+  
+  describe "Se debe poder trabajar con la lista" do
+    it "Se puede acceder al primero" do
+        expect(@l.front()).to eq("Primer punto")
+    end
+    it "Se puede acceder al ultimo" do
+        expect(@l.back()).to eq("Septimo punto")
+    end
+    it "Se puede añadir elementos al final" do
+        @l.push_back("Octavo punto")
+        expect(@l.back()).to eq("Octavo punto")
+    end
+    it "Se puede añadir elementos al principio" do
+        @l.push_front("Primerisimo punto")
+        expect(@l.front()).to eq("Primerisimo punto")
+    end
+    it "Se puede acceder a un elemento i" do
+        expect(@l.element(3)).to eq("Tercer punto")
+        expect(@l.element(0)).to eq("Primerisimo punto")
+        expect(@l.element(8)).to eq("Octavo punto")
+        expect(@l.element(5)).to eq("Quinto punto")
+    end
+    it "Se puede añadir un elemento en la posición i" do
+        @l.set_element("Segundo y medio punto", 3)
+        expect(@l.element(2)).to eq("Segundo punto")
+        expect(@l.element(3)).to eq("Segundo y medio punto")
+        expect(@l.element(4)).to eq("Tercer punto")
+    end
+    it "Se puede eliminar el primer elemento" do
+        expect(@l.pop_front()).to eq("Primerisimo punto")
+        expect(@l.front).to eq("Primer punto")
+    end
+    it "Se puede eliminar el ultimo elemento" do
+        expect(@l.pop_back()).to eq("Octavo punto")
+        expect(@l.back()).to eq("Septimo punto")
+    end
+    it "Se puede eliminar el elemento i" do
+        expect(@l.pop_element(2)).to eq("Segundo y medio punto")
+        expect(@l.element(2)).to eq("Tercer punto")
+        expect(@l.element(1)).to eq("Segundo punto")
+    end
+    
+    it "Tiene salida de texto" do
+        expect(@l.to_s).to eq("{\nPrimer punto\nSegundo punto\nTercer punto\nCuarto punto\nQuinto punto\nSexto punto\nSeptimo punto\n}")
+    end
+  
+  end
+  
 end
+
